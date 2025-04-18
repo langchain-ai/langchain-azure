72c8ee9fc2f2d4f6885dde6dec00ee690fe72ab5
anushakolan <anushakolan10@gmail.com>
2025-04-18 01:05:02 -0700

    Fixing duplicate code added because of merge

--- libs/sqlserver/tests/integration_tests/test_vectorstores.py
+++ libs/sqlserver/tests/integration_tests/test_vectorstores.py
@@ -8,7 +8,6 @@ from unittest.mock import Mock
 import pytest
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sqlalchemy import create_engine, text
 
 from langchain_sqlserver.vectorstores import DistanceStrategy, SQLServer_VectorStore
@@ -30,9 +29,6 @@ from tests.utils.filtering_test_cases import (
     texts as filter_texts,
 )
 
-# pytest.skip(
-#     "Skipping these tests pending resource availability", allow_module_level=True
-# )
 # pytest.skip(
 #     "Skipping these tests pending resource availability", allow_module_level=True
 # )
@@ -100,7 +96,6 @@ def store() -> Generator[SQLServer_VectorStore, None, None]:
         # size as `embedding_length`.
         embedding_function=DeterministicFakeEmbedding(size=EMBEDDING_LENGTH),
         table_name=_TABLE_NAME,
-        batch_size=200,
         batch_size=200,
     )
     yield store  # provide this data to the test
@@ -961,6 +956,5 @@ def connect_to_vector_store(
         # size as `embedding_length`.
         embedding_function=DeterministicFakeEmbedding(size=EMBEDDING_LENGTH),
         table_name=_TABLE_NAME,
-        batch_size=batch_size,
         batch_size=batch_size,
     )
