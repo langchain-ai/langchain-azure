6b60a8cc8219b452e1749ef6381446ca523f7c09
Marlene <57748216+marlenezw@users.noreply.github.com>
2025-06-09 11:17:18 +0000

    adding in package files and poetry file.

---
+++ libs/azure-storage/langchain_azure_storage/document_loaders/base.py
@@ -0,0 +1,8 @@
+"""Base classes for document loaders in LangChain Azure Storage."""
+
+from langchain_core.document_loaders import BaseBlobParser, BaseLoader
+
+__all__ = [
+    "BaseBlobParser",
+    "BaseLoader",
+]
