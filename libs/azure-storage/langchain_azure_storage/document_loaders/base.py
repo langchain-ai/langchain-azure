501bd0ae115672722e4f3e99b5d192b3be37d07a
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-06-16 13:06:10 -0700

    removed document loaders folder

--- libs/azure-storage/langchain_azure_storage/document_loaders/base.py
+++
@@ -1,8 +0,0 @@
-"""Base classes for document loaders in LangChain Azure Storage."""
-
-from langchain_core.document_loaders import BaseBlobParser, BaseLoader
-
-__all__ = [
-    "BaseBlobParser",
-    "BaseLoader",
-]
