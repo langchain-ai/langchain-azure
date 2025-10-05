b96935d9af5a202728c1308ab7e94ba6187094ee
Facundo Santiago <fasantia@microsoft.com>
2025-10-05 23:40:31 +0000

    fix: tests

--- libs/azure-ai/langchain_azure_ai/utils/env.py
+++ libs/azure-ai/langchain_azure_ai/utils/env.py
@@ -6,23 +6,6 @@ import os
 from typing import Any, Optional, Union
 
 
-def env_var_is_set(env_var: str) -> bool:
-    """Check if an environment variable is set.
-
-    Args:
-        env_var (str): The name of the environment variable.
-
-    Returns:
-        bool: True if the environment variable is set, False otherwise.
-    """
-    return env_var in os.environ and os.environ[env_var] not in {
-        "",
-        "0",
-        "false",
-        "False",
-    }
-
-
 def get_from_dict_or_env(
     data: dict[str, Any],
     key: Union[str, list[str]],
