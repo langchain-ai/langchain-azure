9ee891794718672ca36f51e494b6affdf502f940
Kyle Knapp <kyleknapp@microsoft.com>
2025-10-16 14:20:40 -0700

    Add azure storage support for LangChain 1.0
    Changes include:
    * Updating package version to be 1.0 release candidate
    * Remove support for Python 3.9 since LangChain 1.0 drops support
    * Update dependencies to rely on langchain-core 1.0 release candidate
    * Update package classifier
    * Add beta decorator to document loader. This will allow us to get
      feedback on the interface before marking it as stable.

--- libs/azure-storage/langchain_azure_storage/document_loaders.py
+++ libs/azure-storage/langchain_azure_storage/document_loaders.py
@@ -20,6 +20,7 @@ import azure.identity.aio
 from azure.storage.blob import BlobClient, BlobProperties, ContainerClient
 from azure.storage.blob.aio import BlobClient as AsyncBlobClient
 from azure.storage.blob.aio import ContainerClient as AsyncContainerClient
+from langchain_core._api import beta
 from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents.base import Document
 from langchain_core.runnables.config import run_in_executor
@@ -35,6 +36,12 @@ _SDK_CREDENTIAL_TYPE = Optional[
 ]
 
 
+@beta(
+    message=(
+        "`AzureBlobStorageLoader` is in public preview. "
+        "Its API is not stable and may change in future versions."
+    )
+)
 class AzureBlobStorageLoader(BaseLoader):
     """Document loader for LangChain Document objects from Azure Blob Storage."""
 
